2aec49d2c1a2becefe00bc84d378128677954310
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-06-09 07:20:22 -0400

    Update __init__.py

--- modmail/__init__.py
+++ modmail/__init__.py
@@ -5,4 +5,4 @@ __red_end_user_data_statement__ = "This cog does not store any End User Data."
 
 
 async def setup(bot):
-    await bot.add_cog(AltDentifier(bot))
+    await bot.add_cog(ModMail(bot))
